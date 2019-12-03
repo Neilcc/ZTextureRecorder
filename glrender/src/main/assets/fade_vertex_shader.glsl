cc2418cd3808e2fbece76fd2b9400dbd9071a831
zhuchengcheng <zhuchengcheng@netease.corp.com>
2019-12-03 15:38:25 +0800

    add_steam_style_shader

--- glrender/src/main/assets/fade_vertex_shader.glsl
+++ glrender/src/main/assets/fade_vertex_shader.glsl
@@ -6,6 +6,5 @@ varying vec2 vTextureCoord;
 
 void main() {
     gl_Position = uMVPMatrix * aPosition;
-    //    vTextureCoord = (uTexMatrix * aTextureCoord).xy;
     vTextureCoord = (uTexMatrix * aTextureCoord).xy;
 }
